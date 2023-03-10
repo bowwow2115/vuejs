8cfe55ce537e6cc37bcd7d8d5042c028ef762a37
bowwow2115 <bowwow2115@gmail.com>
2023-03-10 17:08:48 +0900

    event 받는 법

---
+++ basics-03-events-starting-code/app.js
@@ -0,0 +1,29 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+      name: "",
+      confirmedName: "",
+    };
+  },
+  methods: {
+    add(num) {
+      this.counter = this.counter + num;
+    },
+    reduce(num) {
+      if (this.counter <= 0) return;
+      this.counter = this.counter - num;
+    },
+    setName(event, lastName) {
+      this.name = event.target.value + " " + lastName;
+    },
+    submitForm() {
+      alert("Submitted");
+    },
+    confirmInput() {
+      this.confirmedName = this.name;
+    },
+  },
+});
+
+app.mount("#events");
