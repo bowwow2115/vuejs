21013802e84c244816aeab1dd4ab59335a838680
bowwow2115 <bowwow2115@gmail.com>
2023-03-17 18:44:20 +0900

    refs 적용

--- section5/lecture/behind-scenes-01-starting-setup/app.js
+++ section5/lecture/behind-scenes-01-starting-setup/app.js
@@ -10,9 +10,29 @@ const app = Vue.createApp({
       this.currentUserInput = event.target.value;
     },
     setText() {
-      this.message = this.currentUserInput;
+      this.message = this.$refs.userText.value;
+      // this.message = this.currentUserInput;
     },
   },
 });
 
 app.mount("#app");
+
+const data = {
+  message: "Hello!",
+  longMessage: "Hello World!",
+};
+
+const handler = {
+  set(target, key, value) {
+    console.log(target);
+    console.log(key);
+    console.log(value);
+  },
+};
+
+const proxy = new Proxy(data, handler);
+
+proxy.message = "Hello!!!";
+
+console.log(proxy);
