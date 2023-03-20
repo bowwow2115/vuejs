867c47f9152ca6cbd0b10f450803bc3af4ae91dd
bowwow2115 <bowwow2115@gmail.com>
2023-03-20 13:29:18 +0900

    컴포넌트로 변경

--- section6/cmp-intro-01-starting-setup/app.js
+++ section6/cmp-intro-01-starting-setup/app.js
@@ -27,20 +27,20 @@ const app = Vue.createApp({
 
 app.component("friend-contact", {
   template: `
-         <li v-for="friend in friends" :key="friend.id">
-          <h2>{{friend.name}}</h2>
-          <button @click="toggleDetails()">
-            {{detailsAreVisible ? 'Hide' : 'Show'}} Details
-          </button>
-          <ul v-if="detailsAreVisible">
-            <li><strong>Phone:</strong> {{friend.phone}}</li>
-            <li><strong>Email:</strong> {{friend.email}}</li>
-          </ul>
-        </li>
-  `,
+           <li>
+            <h2>{{friend.name}}</h2>
+            <button @click="toggleDetails()">
+              {{detailsAreVisible ? 'Hide' : 'Show'}} Details
+            </button>
+            <ul v-if="detailsAreVisible">
+              <li><strong>Phone:</strong> {{friend.phone}}</li>
+              <li><strong>Email:</strong> {{friend.email}}</li>
+            </ul>
+          </li>
+    `,
   data() {
     return {
-      detailAreVisible: false,
+      detailsAreVisible: false,
       friend: {
         id: "park",
         name: "SH Park",
