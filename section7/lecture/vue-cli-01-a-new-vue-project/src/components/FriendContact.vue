b7a4cb9c67c3d349033f733d2a43aa4ebca00be4
bowwow2115 <bowwow2115@gmail.com>
2023-03-21 14:16:06 +0900

    component 추가

---
+++ section7/lecture/vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,35 @@
+<template>
+  <li>
+    <header><h1>{{ friend.name }}</h1></header>
+    <button @click="toggleDetails()">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone: </strong>{{ friend.phone }}</li>
+      <li><strong>Email: </strong>{{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+          id: 'park',
+          name: 'SH Park',
+          phone: '010 1111 1111',
+          email: 'shpark@google.com'
+        },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    }
+  }
+};
+</script>
+
+<style>
+
+</style>
