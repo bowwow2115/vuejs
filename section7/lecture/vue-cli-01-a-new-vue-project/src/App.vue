81e703b66549056cf37abfaacbad4d755411bd91
bowwow2115 <bowwow2115@gmail.com>
2023-03-21 13:31:26 +0900

    vue app init

---
+++ section7/lecture/vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <section>
+  <h2>My Friends</h2>
+  <ul>
+    <li>asdf</li>
+  </ul>
+</section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: 'park',
+          name: 'SH Park',
+          phone: '010 1111 1111',
+          email: 'shpark@google.com'
+        },
+        {
+          id: 'song',
+          name : "KH Song",
+          phone: "010 2222 2222", 
+          email: "khsong@google.com"
+        }
+        
+      ]
+    }
+  }
+}
+</script>
+
+
