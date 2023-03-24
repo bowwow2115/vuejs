60850aff2a1b3b8f050525dc134a48b7a6ca8c8e
bowwow2115 <bowwow2115@gmail.com>
2023-03-24 15:44:19 +0900

    prettier 적용

--- section8/cmp-adv-01-starting-setup/src/App.vue
+++ section8/cmp-adv-01-starting-setup/src/App.vue
@@ -1,35 +1,35 @@
 <template>
-  <div>
-    <the-header></the-header>
-    <badge-list></badge-list>
-    <user-info
-      :full-name="activeUser.name"
-      :info-text="activeUser.description"
-      :role="activeUser.role"
-    ></user-info>
-  </div>
+	<div>
+		<the-header></the-header>
+		<badge-list></badge-list>
+		<user-info
+			:full-name="activeUser.name"
+			:info-text="activeUser.description"
+			:role="activeUser.role"
+		></user-info>
+	</div>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      activeUser: {
-        name: 'Maximilian Schwarzmüller',
-        description: 'Site owner and admin',
-        role: 'admin',
-      },
-    };
-  },
-};
+	export default {
+		data() {
+			return {
+				activeUser: {
+					name: "Maximilian Schwarzmüller",
+					description: "Site owner and admin",
+					role: "admin",
+				},
+			};
+		},
+	};
 </script>
 
 <style>
-html {
-  font-family: sans-serif;
-}
+	html {
+		font-family: sans-serif;
+	}
 
-body {
-  margin: 0;
-}
+	body {
+		margin: 0;
+	}
 </style>
