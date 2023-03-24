8f0029558b2ae0acf522a2a203209caec01b7de5
bowwow2115 <bowwow2115@gmail.com>
2023-03-24 15:21:48 +0900

    prettier 적용

--- section8/cmp-adv-01-starting-setup/.eslintrc.js
+++ section8/cmp-adv-01-starting-setup/.eslintrc.js
@@ -1,17 +1,35 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended'
-  ],
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
-}
+	root: true,
+	env: {
+		node: true,
+	},
+	extends: [
+		"plugin:vue/vue3-essential",
+		"eslint:recommended",
+		"prettier",
+		"plugin:prettier/recommended",
+	],
+	plugins: ["prettier"],
+	parserOptions: {
+		parser: "babel-eslint",
+	},
+	rules: {
+		"no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+		"no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+		"prettier/prettier": [
+			"error",
+			// 아래 규칙들은 개인 선호에 따라 prettier 문법 적용
+			// https://prettier.io/docs/en/options.html
+			{
+				singleQuote: true,
+				semi: true,
+				useTabs: true,
+				tabWidth: 2,
+				trailingComma: "all",
+				printWidth: 80,
+				bracketSpacing: true,
+				arrowParens: "avoid",
+			},
+		],
+	},
+};
---
+++ section8/cmp-adv-01-starting-setup/src/components/BaseCard.vue
@@ -0,0 +1,9 @@
+<template><div></div></template>
+
+<script>
+	export default {
+		props: ["content"],
+	};
+</script>
+
+<style></style>
