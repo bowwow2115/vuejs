a63a51c9f219dab6d5344523bee17d654432c32b
bowwow2115 <bowwow2115@gmail.com>
2023-03-28 11:13:52 +0900

    Base Button 추가

--- section9/prj-cmp-01-starting-setup/src/main.js
+++ section9/prj-cmp-01-starting-setup/src/main.js
@@ -2,7 +2,9 @@ import App from './App.vue';
 import { createApp } from 'vue';
 
 import Basecard from './components/UI/BaseCard.vue';
+import BaseButton from './components/UI/BaseButton.vue';
 
 const app = createApp(App);
 app.component('base-card', Basecard);
+app.component('base-button', BaseButton);
 app.mount('#app');
