38f64105c53e3f05f2d7147156261dcc843210aa
bowwow2115 <bowwow2115@gmail.com>
2023-03-29 16:33:18 +0900

    유효성 검사 추가

--- section10/forms-01-starting-setup/src/components/TheForm.vue
+++ section10/forms-01-starting-setup/src/components/TheForm.vue
@@ -1,8 +1,18 @@
 <template>
   <form @submit.prevent="submitForm">
-    <div class="form-control">
+    <div
+      class="form-control"
+      :class="{ invalid: userNameValidity === 'invalid' }"
+    >
       <label for="user-name">Your Name</label>
-      <input id="user-name" name="user-name" type="text" v-model="userName" />
+      <input
+        id="user-name"
+        name="user-name"
+        type="text"
+        v-model.trim="userName"
+        @blur="validateInput"
+      />
+      <p v-if="userNameValidity === 'invalid'">Please enter a valid</p>
     </div>
     <div class="form-control">
       <label for="age">Your Age (Years)</label>
@@ -83,6 +93,9 @@
       </div>
     </div>
     <div class="form-control">
+      <rating-control v-model="rating"></rating-control>
+    </div>
+    <div class="form-control">
       <input
         type="checkbox"
         id="confirm-terms"
@@ -92,13 +105,15 @@
       <label for="confirm-terms">Agree to terms of use?</label>
     </div>
     <div>
-      <button>Save Data</button>
+      <button @click="submitForm">Save Data</button>
     </div>
   </form>
 </template>
 
 <script>
+import RatingControl from './RatingControl.vue';
 export default {
+  components: { RatingControl },
   data() {
     return {
       userName: '',
@@ -107,6 +122,8 @@ export default {
       interest: [],
       how: null,
       confirm: null,
+      userNameValidity: 'pending',
+      rating: null,
     };
   },
   methods: {
@@ -124,6 +141,15 @@ export default {
       this.interest = [];
       this.how = null;
       this.confirm = null;
+      console.log('rating: ' + this.rating);
+      this.rating = null;
+    },
+    validateInput() {
+      if (this.userName === '') {
+        this.userNameValidity = 'invalid';
+      } else {
+        this.userNameValidity = 'valid';
+      }
     },
   },
 };
@@ -141,6 +167,13 @@ form {
 
 .form-control {
   margin: 0.5rem 0;
+}
+
+.form-control.invalid input {
+  border-color: red;
+}
+.form-control.invalid label {
+  color: red;
 }
 
 label {
