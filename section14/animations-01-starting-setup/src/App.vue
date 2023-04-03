1cbc3084ccaff387cb1d740e28c77ce799274698
bowwow2115 <bowwow2115@gmail.com>
2023-04-03 18:58:53 +0900

    keyframe animation 추가

--- section14/animations-01-starting-setup/src/App.vue
+++ section14/animations-01-starting-setup/src/App.vue
@@ -60,7 +60,7 @@ button:active {
   height: 8rem;
   background-color: #290033;
   margin-bottom: 2rem;
-  transition: transform;
+  transition: transform 0.3s ease-out;
 }
 .container {
   max-width: 40rem;
@@ -74,6 +74,21 @@ button:active {
   border-radius: 12px;
 }
 .animate {
-  transform: translateX(-50px);
+  /* transform: translateX(-50px); */
+  animation: slide-fade 0.3s ease-out forwards;
+}
+
+@keyframes slide-fade {
+  0% {
+    transform: translateX(0) scale(1);
+  }
+
+  70% {
+    transform: translate(-120px) scale(1.1);
+  }
+
+  100% {
+    transform: translate(-150px) scale(1);
+  }
 }
 </style>
