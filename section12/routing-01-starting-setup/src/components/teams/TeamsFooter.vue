800a52cc66801e9dc797d979d68dc0057884a538
bowwow2115 <bowwow2115@gmail.com>
2023-04-03 17:54:45 +0900

    디렉토리 구조 변경

--- section12/routing-01-starting-setup/src/components/teams/TeamsFooter.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <h2>Teams Footer</h2>
-</template>
-
-<script>
-export default {};
-</script>
-
-<style></style>
--- section12/routing-01-starting-setup/src/components/users/UsersFooter.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <h2>Users Footer</h2>
-</template>
-
-<script>
-export default {};
-</script>
-
-<style></style>
