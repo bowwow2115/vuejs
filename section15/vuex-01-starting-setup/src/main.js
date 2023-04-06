a54cc7518d05843112c1e338e3b1c6ab3bfd007a
bowwow2115 <bowwow2115@gmail.com>
2023-04-06 18:23:21 +0900

    vuex mutation getter 추가

--- section15/vuex-01-starting-setup/src/main.js
+++ section15/vuex-01-starting-setup/src/main.js
@@ -1,7 +1,41 @@
 import { createApp } from 'vue';
+import { createStore } from 'vuex';
 
 import App from './App.vue';
 
+const store = createStore({
+  state() {
+    return {
+      counter: 0,
+    };
+  },
+  mutations: {
+    addOne(state) {
+      state.counter++;
+    },
+    increase(state, payload) {
+      state.counter = state.counter + payload.value;
+    },
+  },
+  getters: {
+    finalCounter(state) {
+      return state.counter * 2;
+    },
+    normalizedConuter(state, getters) {
+      const finalCounter = getters.finalCounter;
+      if (finalCounter < 0) {
+        return 0;
+      }
+      if (finalCounter > 100) {
+        return 100;
+      }
+      return finalCounter;
+    },
+  },
+});
+
 const app = createApp(App);
+
+app.use(store);
 
 app.mount('#app');
