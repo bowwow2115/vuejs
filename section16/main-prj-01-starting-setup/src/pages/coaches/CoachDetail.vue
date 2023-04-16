3396bdb1a42323dd2813f2c191b543d7cec99de6
bowwow2115 <bowwow2115@gmail.com>
2023-04-16 15:46:50 +0900

    Componet 생성 및router mapping

---
+++ section16/main-prj-01-starting-setup/src/pages/coaches/CoachDetail.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>Details For Coach</div>
+  <router-link to="/coaches/c1/contact"></router-link>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ section16/main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
@@ -0,0 +1,21 @@
+<template>
+  <section>
+    FILTER
+  </section>
+  <section>
+    <div class="controls">
+      <button>Refresh</button>
+      <router-link to="/register">Register as Coach</router-link>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
