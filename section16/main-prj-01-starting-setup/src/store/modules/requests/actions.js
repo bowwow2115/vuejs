38185c936e04976f9d12de9079973fd17745e733
bowwow2115 <bowwow2115@gmail.com>
2023-05-07 14:32:33 +0900

    Requset 서버 조회 추가 및 에러 다이어로그 추가

--- section16/main-prj-01-starting-setup/src/store/modules/requests/actions.js
+++ section16/main-prj-01-starting-setup/src/store/modules/requests/actions.js
@@ -1,11 +1,53 @@
 export default {
-  contactCoach(context, payload) {
+  async contactCoach(context, payload) {
     const newRequest = {
-      id: new Date().toISOString(),
-      coachId: payload.coachId,
       userEmail: payload.email,
       message: payload.message,
     };
+    const response = await fetch(
+      `https://vue-app-01-23c07-default-rtdb.asia-southeast1.firebasedatabase.app/requests/${payload.coachId}.json`,
+      {
+        method: 'POST',
+        body: JSON.stringify(newRequest),
+      }
+    );
+
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      throw new Error(responseData.message || 'Failed to send request.');
+    }
+
+    newRequest.id = responseData.name;
+    newRequest.coachId = payload.coachId;
+
     context.commit('addRequest', newRequest);
+  },
+  async fetchRequests(context) {
+    const coachId = context.rootGetters.userId;
+    const response = await fetch(
+      `https://vue-app-01-23c07-default-rtdb.asia-southeast1.firebasedatabase.app/requests/${coachId}.json`
+    );
+    const responseData = await response.json();
+    console.log(responseData);
+
+    if (!(await response).ok) {
+      throw new Error(responseData.message || 'Failed to fetch request.');
+    }
+
+    const requests = [];
+
+    for (const key in responseData) {
+      const request = {
+        id: key,
+        coachId: coachId,
+        userEmail: responseData[key].userEmail,
+        message: responseData[key].message,
+      };
+      requests.push(request);
+    }
+    console.log(requests);
+
+    context.commit('setRequests', requests);
   },
 };
