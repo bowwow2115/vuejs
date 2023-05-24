b0435daa3151d31e105e430033df8af696bd9f3e
bowwow2115 <bowwow2115@gmail.com>
2023-05-24 17:03:24 +0900

    오토로그아웃 추가 중

--- section16/main-prj-01-starting-setup/src/store/modules/auth/actions.js
+++ section16/main-prj-01-starting-setup/src/store/modules/auth/actions.js
@@ -1,3 +1,4 @@
+let timer;
 export default {
   async auth(context, payload) {
     const mode = payload.mode;
@@ -26,24 +27,35 @@ export default {
       throw error;
     }
 
+    const expiresIn = responseData.expiresIn * 1000;
+    const expirationDate = new Date().getTime() + expiresIn;
+
     localStorage.setItem('token', responseData.idToken);
     localStorage.setItem('userId', responseData.localId);
+    localStorage.setItem('tokenExpiration', expirationDate);
+
+    timer = setTimeout(() => context.dispatch('logout'), expiresIn);
 
     context.commit('setUser', {
       token: responseData.idToken,
       userId: responseData.localId,
-      tokenExpiration: responseData.expiresIn,
     });
   },
   autoLogin(context) {
     const token = localStorage.getItem('token');
     const userId = localStorage.getItem('userId');
+    const tokenExpiration = localStorage.getItem('tokenExpiration');
+
+    const expiresIn = +tokenExpiration - new Date().getTime();
+
+    if (expiresIn < 0) {
+      return;
+    }
 
     if (token && userId) {
       context.commit('setUser', {
         token: token,
         userId: userId,
-        tokenExpiration: null,
       });
     }
   },
@@ -60,10 +72,15 @@ export default {
     });
   },
   logout(context) {
+    localStorage.removeItem('token');
+    localStorage.removeItem('userId');
+    localStorage.removeItem('tokenExpiration');
+
+    clearTimeout(timer);
+
     context.commit('setUser', {
       token: null,
       userId: null,
-      tokenExpiration: null,
     });
   },
 };
