1dfb34ee20c1c79a53681838736d84650df0ba0c
bowwow2115 <bowwow2115@gmail.com>
2023-05-24 16:36:20 +0900

    자동 로그인 추가

--- section16/main-prj-01-starting-setup/src/store/modules/auth/actions.js
+++ section16/main-prj-01-starting-setup/src/store/modules/auth/actions.js
@@ -1,60 +1,62 @@
 export default {
-  async login(context, payload) {
-    const response = await fetch(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAeKXv6Dniti_v5OIbKn5EMkK-0X8pLgmk',
-      {
-        method: 'POST',
-        body: JSON.stringify({
-          email: payload.email,
-          password: payload.password,
-          returnSecureToken: true,
-        }),
-      }
-    );
+  async auth(context, payload) {
+    const mode = payload.mode;
+    let url =
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAeKXv6Dniti_v5OIbKn5EMkK-0X8pLgmk';
+
+    if (mode === 'signup') {
+      url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAeKXv6Dniti_v5OIbKn5EMkK-0X8pLgmk';
+    }
+    const response = await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({
+        email: payload.email,
+        password: payload.password,
+        returnSecureToken: true,
+      }),
+    });
 
     const responseData = await response.json();
 
     if (!response.ok) {
-      console.log(responseData);
       const error = new Error(
         responseData.message || 'Failed to authenticate. Check your login data.'
       );
       throw error;
     }
 
-    console.log(responseData);
+    localStorage.setItem('token', responseData.idToken);
+    localStorage.setItem('userId', responseData.localId);
+
     context.commit('setUser', {
       token: responseData.idToken,
       userId: responseData.localId,
       tokenExpiration: responseData.expiresIn,
     });
   },
-  async signup(context, payload) {
-    console.log(payload);
-    const response = await fetch(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAeKXv6Dniti_v5OIbKn5EMkK-0X8pLgmk',
-      {
-        method: 'POST',
-        body: JSON.stringify({
-          email: payload.email,
-          password: payload.password,
-          returnSecureToken: true,
-        }),
-      }
-    );
+  autoLogin(context) {
+    const token = localStorage.getItem('token');
+    const userId = localStorage.getItem('userId');
 
-    const responseData = await response.json();
-
-    if (!response.ok) {
-      console.log(responseData);
-      const error = new Error(responseData.message || 'Failed to authenticate');
-      throw error;
+    if (token && userId) {
+      context.commit('setUser', {
+        token: token,
+        userId: userId,
+        tokenExpiration: null,
+      });
     }
-    console.log(responseData);
-    context.commit('setUser', {
-      token: responseData.idToken,
-      userId: responseData.localId,
-      tokenExpiration: responseData.expiresIn,
+  },
+  async login(context, payload) {
+    context.dispatch('auth', {
+      ...payload,
+      mode: 'login',
+    });
+  },
+  async signup(context, payload) {
+    context.dispatch('auth', {
+      ...payload,
+      mode: 'signup',
     });
   },
   logout(context) {
